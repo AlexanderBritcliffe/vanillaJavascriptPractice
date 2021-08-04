d807ecc35b0d65ff068c6cb2014bcb5c72edd879
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2021-08-04 00:25:25 -0400

    four solutions for duplicate check

---
+++ practice.js
@@ -0,0 +1,90 @@
+console.log(
+  isUnique('abcdef'),
+  isUnique('89%df#$^a&x'),
+  isUnique('abcAdef'),
+  isUnique('abcaef'),
+
+);
+
+function isUnique(str) {
+
+  for(let i=0; i < str.length; i++) { //process every character in loop
+    if(str.lastIndexOf(str[i]) !== i) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//line 10 loops from left to right
+//line 11 goes through string from right to
+//left so for example initially it will go from left to right looking for a and so on
+//will return false unless a is found in another position other than first
+
+console.log(
+  isUnique('abcdef'),
+  isUnique('89%df#$^a&x'),
+  isUnique('abcAdef'),
+  isUnique('abcaef'),
+
+);
+
+function isUnique(str) {
+  const chars = str.split('').sort(); //splits into an array and sort them from left to right dbac would be a, b, c, d
+
+
+  for(let i=1; i <= chars.length; i++) { //dbaca would be aabcd---thats why i starts at one or second character so it can compare to interger next to it
+    if(chars[i] === chars[i -1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(
+  isUnique('abcdef'),
+  isUnique('89%df#$^a&x'),
+  isUnique('abcAdef'),
+  isUnique('abcaef'),
+
+);
+
+function isUnique(str) {
+  const chars = {}
+
+  for(let i=0; i < str.length; i++) {
+    const thisChar = str[i]; //the first time we go through loop thisChar
+    // will equal a...then it will check if the object contains a and at that point it will be empty so will return
+    //true since object does not contain it (line 61)....we then move to line 63 we store a in object and set its value to true
+
+    if(chars[thisChar] === true) {
+      return false;
+    }
+    chars[thisChar] = true;
+  }
+  return true; //this is for if we get through the entire thing and there are no duplicates
+}
+
+console.log(
+  isUnique('abcdef'),
+  isUnique('89%df#$^a&x'),
+  isUnique('abcAdef'),
+  isUnique('abcaef'),
+
+);
+
+function isUnique(str) {
+  const chars = new Set();
+
+  for(let i=0; i < str.length; i++) {
+    const thisChar = str[i]; //the first time we go through loop thisChar
+    // will equal a...then it will check if the object contains a and at that point it will be empty so will return
+    //true since object does not contain it (line 61)....we then move to line 63 we store a in object and set its value to true
+
+    if(chars.has(thisChar)) {
+      return false;
+    }
+    chars.add(thisChar)
+  }
+  return true; //this is for if we get through the entire thing and there are no duplicates
+}
