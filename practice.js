b522f684bdf9113e3204506bf9f2745d07b6ecf0
Alexander Britcliffe <alexanderbritcliffe@gmail.com>
2022-02-10 01:14:22 -0500

    single mutation

--- practice.js
+++ practice.js
@@ -284,3 +284,19 @@ function maximumProfits(prices) {
     }
   }
 }
+
+function singleMutation(str1, str2) {
+  let mutations = 0;
+  for(let i = 0, j = 0; i < str.length || j < str2.length; i++, j++)
+  if(str1[i] !== str2[j]) {
+    mutations++;
+
+    if(mutations > 1) {
+      return false;
+    }
+
+    if(str1.length > str2.length) {
+      j--;
+    }
+  }
+}
